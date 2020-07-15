d0d8b6f93224a528d3549250363fe2dd55beba99
taesikyoo <yrns0907@gmail.com>
2020-07-16 00:49:29 +0900

    3장: 스프링 부트에서 JPA로 데이터베이스 다뤄보자

---
+++ src/main/java/com/taesikyoo/book/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package com.taesikyoo.book.springboot.web.dto;
+
+import com.taesikyoo.book.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
